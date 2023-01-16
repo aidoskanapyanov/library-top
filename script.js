0c3862945d2423fc78b652656a2aa70d578ab949
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-01-16 12:09:57 +0600

    feat: add books through form

--- script.js
+++ script.js
@@ -17,25 +17,67 @@ function addBookToLibrary(book) {
 
 let books = document.querySelector(".books");
 
-myLibrary.forEach((book) => {
-  let card = document.createElement("div");
-  card.classList.add("book");
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    let card = document.createElement("div");
+    card.classList.add("book");
 
-  let title = document.createElement("h4");
-  title.innerText = book.title;
-  card.appendChild(title);
+    let title = document.createElement("h4");
+    title.innerText = book.title;
+    card.appendChild(title);
 
-  let author = document.createElement("p");
-  author.innerText = `by ${book.author}`;
-  card.appendChild(author);
+    let author = document.createElement("p");
+    author.innerText = `by ${book.author}`;
+    card.appendChild(author);
 
-  let pages = document.createElement("p");
-  pages.innerText = `${book.pages} pages`;
-  card.appendChild(pages);
+    let pages = document.createElement("p");
+    pages.innerText = `${book.pages} pages`;
+    card.appendChild(pages);
 
-  let read = document.createElement("p");
-  read.innerText = book.read ? "Read" : "Not Read";
-  card.appendChild(read);
+    let read = document.createElement("p");
+    read.innerText = book.read ? "Read" : "Not Read";
+    card.appendChild(read);
 
-  books.appendChild(card);
+    books.appendChild(card);
+  });
+}
+displayBooks();
+
+let modal = document.getElementById("myModal");
+let bookBtn = document.getElementById("bookBtn");
+let closeBtn = document.getElementsByClassName("close")[0];
+let submitBtn = document.getElementById("submitBtn");
+
+bookBtn.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+submitBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+let form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  let book = new Book(
+    (title = data.get("title")),
+    (author = data.get("author")),
+    (pages = data.get("pages")),
+    (read = data.get("read") == "true" ? true : false)
+  );
+  addBookToLibrary(book);
+  books.innerHTML = "";
+  displayBooks();
 });
